5307bbaacdabd1bae9812ffa49dbf06cd838bae4
Fraser McLean <dev@frasermclean.com>
2022-02-21 10:46:10 +1100

    Added required data annotation

--- WhatTheWord.Domain/Processing/Clients/RegisterClientRequest.cs
+++ WhatTheWord.Domain/Processing/Clients/RegisterClientRequest.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 namespace WhatTheWord.Domain.Processing.Clients;
 
 public class RegisterClientRequest : IRequest<RegisterClientResponse>
 {
+    [Required]
     public string Fingerprint { get; init; } = default!;
 }
