11dfb5dcef121650c25a37a6acae526b5f1bb122
Fraser McLean <dev@frasermclean.com>
2022-04-04 12:03:58 +1000

    Improved guess component

--- Client/src/app/game/guess/guess.component.ts
+++ Client/src/app/game/guess/guess.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { GuessResponse } from 'src/app/models/guess.response';
 import { GameService } from 'src/app/services/game.service';
 
@@ -8,19 +17,49 @@ import { GameService } from 'src/app/services/game.service';
   templateUrl: './guess.component.html',
   styleUrls: ['./guess.component.scss'],
 })
-export class GuessComponent implements OnInit {
-  guess = new FormControl('');
+export class GuessComponent implements OnInit, OnDestroy {
+  inputControl = new FormControl('');
   response: GuessResponse | null = null;
+  hint$ = this.gameService.hint$;
+  subscription: Subscription = null!;
+
+  @Output()
+  valueChanged = new EventEmitter<string>();
+
+  @ViewChild('input')
+  inputElement: ElementRef<HTMLInputElement> = null!;
 
   constructor(private gameService: GameService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.inputControl.valueChanges.subscribe((value) => {
+      if (typeof value !== 'string') return;
+      this.valueChanged.emit(value);
+    });
+  }
 
-  async onGuess(value: string) {
-    this.response = await this.gameService.guessWord(value.trim());
-    this.guess.reset();
-    setTimeout(() => {
-      this.response = null;
-    }, 2000);
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onInputBlur(event: FocusEvent) {
+    if (this.inputElement) {
+      this.inputElement.nativeElement.focus();
+    }
+  }
+
+  async onEnterKeyDown() {
+    const value =
+      typeof this.inputControl.value === 'string'
+        ? this.inputControl.value.trim()
+        : '';
+
+    if (value) {
+      this.response = await this.gameService.guessWord(value);
+      this.inputControl.reset('');
+      setTimeout(() => {
+        this.response = null;
+      }, 2000);
+    }
   }
 }
