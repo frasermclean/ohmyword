f3db84f8d4a8ab9afde328e8d86f71ff25083687
Fraser McLean <dev@frasermclean.com>
2022-04-27 18:34:23 +1000

    Added redirect component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-import { MsalBroadcastService, MsalGuard, MsalInterceptor, MsalModule, MsalService } from '@azure/msal-angular';
+import { MsalModule, MsalGuard, MsalInterceptor, MsalRedirectComponent } from '@azure/msal-angular';
 
 import { AppComponent } from './app.component';
 import { msalInstance, guardConfig, interceptorConfig } from './auth-config';
 
 const routes: Routes = [
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule), canActivate: [MsalGuard] },
   { path: 'game', loadChildren: () => import('./game/game.module').then((m) => m.GameModule) },
   { path: '**', redirectTo: 'game', pathMatch: 'full' },
 ];
@@ -22,15 +22,9 @@ const routes: Routes = [
     MsalModule.forRoot(msalInstance, guardConfig, interceptorConfig),
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: MsalInterceptor,
-      multi: true,
-    },
-    MsalService,
-    MsalGuard,
-    MsalBroadcastService,
+    { provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true }, 
+    MsalGuard
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, MsalRedirectComponent],
 })
 export class AppModule {}
