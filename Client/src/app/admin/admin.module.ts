e22a6c7447c80f21b1fc96867fc22535c36ec325
Fraser McLean <dev@frasermclean.com>
2022-07-22 11:29:03 +1000

    Added progress bar to indicate busy status

--- Client/src/app/admin/admin.module.ts
+++ Client/src/app/admin/admin.module.ts
@@ -12,6 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSortModule } from '@angular/material/sort';
@@ -39,6 +40,7 @@ const routes: Routes = [{ path: '', component: AdminContainerComponent }];
     MatInputModule,
     MatMenuModule,
     MatPaginatorModule,
+    MatProgressBarModule,
     MatProgressSpinnerModule,
     MatSelectModule,
     MatSortModule,
