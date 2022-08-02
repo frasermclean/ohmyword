9d175e0c45be8b8c9c35c1a8c9d65e4af2a8fd1c
Fraser McLean <dev@frasermclean.com>
2022-08-02 18:39:45 +1000

    Created UserFunctions class

---
+++ Functions/UserFunctions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+
+namespace OhMyWord.Functions;
+
+public class UserFunctions
+{
+    [FunctionName(nameof(GetUserRoles))]
+    public IActionResult GetUserRoles(
+        [HttpTrigger(methods: "post", Route = "get-roles/{userId}")] HttpRequest request,
+        string userId) 
+    {
+        return new OkObjectResult(userId);
+    }
+}
