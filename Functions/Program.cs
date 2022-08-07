ab7faef73cd8b85921b8e635ba9d106226a28a16
Fraser McLean <dev@frasermclean.com>
2022-08-07 12:31:57 +1000

    Refactored function to use isolated process

---
+++ Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OhMyWord.Data.Options;
+using OhMyWord.Data.Services;
+
+namespace OhMyWord.Functions;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()            
+            .ConfigureAppConfiguration((context, builder) =>
+            {                
+                // add Azure app configuration
+                builder.AddAzureAppConfiguration(options =>
+                {
+                    var endpoint = context.Configuration.GetValue<string>("APP_CONFIG_ENDPOINT");
+                    var uri = new Uri(endpoint);
+                    options.Connect(uri, new DefaultAzureCredential());
+                });
+            })
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHttpClient();
+                collection.Configure<CosmosDbOptions>(context.Configuration.GetSection(CosmosDbOptions.SectionName));                
+                collection.AddSingleton<ICosmosDbService, CosmosDbService>();
+                collection.AddSingleton<IPlayerRepository, PlayerRepository>();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
--- Functions/UserFunctions.cs
+++ Functions/UserFunctions.cs
@@ -1,37 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using OhMyWord.Data.Services;
+using System.Net;
 
 namespace OhMyWord.Functions;
 
 public sealed class UserFunctions
 {
+    private readonly ILogger<UserFunctions> logger;
     private readonly IPlayerRepository playerRepository;
 
-    public UserFunctions(IPlayerRepository playerRepository)
+    public UserFunctions(ILogger<UserFunctions> logger, IPlayerRepository playerRepository)
     {
+        this.logger = logger;
         this.playerRepository = playerRepository;
     }
 
-    [FunctionName(nameof(GetUserRoles))]
-    public async Task<IActionResult> GetUserRoles(
+    [Function(nameof(GetUserRoles))]
+    public async Task<HttpResponseData> GetUserRoles(
         [HttpTrigger("get", Route = "get-roles/{userId:guid}")]
-        HttpRequest request,
-        Guid userId,
-        ILogger logger)
+        HttpRequestData request,
+        Guid userId
+    )
     {
         try
         {
             var player = await playerRepository.FindPlayerByPlayerIdAsync(userId);
             logger.LogInformation("Found player with ID: {UserId}", userId);
-            return new OkObjectResult(player);
+            var response = request.CreateResponse();
+            await response.WriteAsJsonAsync(player);
+            return response;
         }
         catch (Exception ex)
         {
             logger.LogWarning(ex, "Couldn't find player with ID: {UserId}", userId);
-            return new NotFoundResult();
+            return request.CreateResponse(HttpStatusCode.NotFound);
         }
     }
 }
