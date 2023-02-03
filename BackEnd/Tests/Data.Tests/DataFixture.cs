51ff0e4e769d0245d8cbe7cd4de1bca5a7bc7929
Fraser McLean <dev@frasermclean.com>
2023-02-04 10:41:42 +1100

    Added some entity tests

--- BackEnd/Tests/Data.Tests/DataFixture.cs
+++ BackEnd/Tests/Data.Tests/DataFixture.cs
@@ -1,9 +1,21 @@
 ﻿using OhMyWord.Data;
 using OhMyWord.Data.Entities;
+using OhMyWord.Data.Enums;
 
 namespace Data.Tests;
 
 public class DataFixture
 {
-    public WordEntity TestWord { get; } = new() { Id = "test", DefinitionCount = 1, Timestamp = 123 };    
+    public WordEntity TestWord { get; } = new() { Id = "test", DefinitionCount = 1, Timestamp = 123 };
+
+    public DefinitionEntity TestDefinition { get; } = new()
+    {
+        Id = "10271ba9-60ec-4073-8552-14dbb477a895",
+        Value = "Test definition",
+        PartOfSpeech = PartOfSpeech.Noun,
+        Example = "Test example",
+        WordId = "test"
+    };
+
+    public VisitorEntity TestVisitor { get; } = new() { Id = "abc123", RegistrationCount = 3, Score = 400 };
 }
