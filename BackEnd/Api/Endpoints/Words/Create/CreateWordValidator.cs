acfa3bfb5b399aac8104db43d523b6f359755cfb
Fraser McLean <dev@frasermclean.com>
2023-04-01 15:26:10 +1100

    Improved create word request validator

--- BackEnd/Api/Endpoints/Words/Create/CreateWordValidator.cs
+++ BackEnd/Api/Endpoints/Words/Create/CreateWordValidator.cs
@@ -6,6 +6,11 @@ public class CreateWordValidator : Validator<CreateWordRequest>
 {
     public CreateWordValidator()
     {
-        RuleFor(request => request.Id).NotEmpty();
+        RuleFor(request => request.Id)
+            .Cascade(CascadeMode.Stop)
+            .NotEmpty()
+            .Length(3, 16)
+            .Must(id => id.All(Char.IsLetter))
+            .WithMessage("Only single words with no spaces are allowed.");
     }
 }
