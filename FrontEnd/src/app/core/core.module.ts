01d3dde9436c48edc9e1d837927ff34355633bb2
Fraser McLean <dev@frasermclean.com>
2023-04-05 14:55:29 +1000

    Added user menu to toolbar

--- FrontEnd/src/app/core/core.module.ts
+++ FrontEnd/src/app/core/core.module.ts
@@ -5,6 +5,8 @@ import { RouterModule } from '@angular/router';
 // angular material modules
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 // local module components
@@ -13,7 +15,15 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [ToolbarComponent, FooterComponent],
-  imports: [CommonModule, MatButtonModule, MatDividerModule, MatToolbarModule, RouterModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    RouterModule,
+  ],
   exports: [ToolbarComponent, FooterComponent],
 })
 export class CoreModule {}
