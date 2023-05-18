d4d861712715d13e1bbeb5de102269bd00099ef2
Fraser McLean <dev@frasermclean.com>
2023-05-18 14:52:20 +1000

    Created Session class

---
+++ BackEnd/Domain/Models/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace OhMyWord.Domain.Models;
+
+public class Session
+{
+    public Guid Id { get; init; } 
+    public int RoundCount { get; set; }
+    public DateTime StartDate { get; } = DateTime.UtcNow;
+    public DateTime EndDate { get; set; }
+
+    public static Session Default => new();
+}
