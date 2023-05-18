a5bf3fbb7ea735f7650d851c171dd6dfedbc8ee2
Fraser McLean <dev@frasermclean.com>
2023-05-18 13:25:56 +1000

    Added constant for PostRoundDelayDefault

--- BackEnd/Domain/Options/RoundOptions.cs
+++ BackEnd/Domain/Options/RoundOptions.cs
@@ -6,9 +6,10 @@ public class RoundOptions
 {
     public const string SectionName = "Round";
     public const double LetterHintDelayDefault = 3;
+    private const double PostRoundDelayDefault = 5;
     public const int GuessLimitDefault = 3;
 
     [Range(1, 10)] public double LetterHintDelay { get; init; } = LetterHintDelayDefault;
-    [Range(1, 60)] public double PostRoundDelay { get; init; } = 5;
+    [Range(1, 60)] public double PostRoundDelay { get; init; } = PostRoundDelayDefault;
     [Range(1, 10)] public int GuessLimit { get; init; } = GuessLimitDefault;
 }
