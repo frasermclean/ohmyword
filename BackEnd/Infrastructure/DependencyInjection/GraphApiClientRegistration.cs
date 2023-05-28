3fd340ff81d34caa99c1d0184b459c481ce070e0
Fraser McLean <dev@frasermclean.com>
2023-05-28 12:44:48 +1000

    Added graph API client options

---
+++ BackEnd/Infrastructure/DependencyInjection/GraphApiClientRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OhMyWord.Infrastructure.Options;
+
+namespace OhMyWord.Infrastructure.DependencyInjection;
+
+public static class GraphApiClientRegistration
+{
+    public static IServiceCollection AddGraphApiClient(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddOptions<GraphApiClientOptions>()
+            .Bind(configuration.GetSection(GraphApiClientOptions.SectionName))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        return services;
+    }
+}
