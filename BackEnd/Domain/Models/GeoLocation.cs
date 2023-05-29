0b0c11cb13d6517c827af89f07b1e9fa23c14de1
Fraser McLean <dev@frasermclean.com>
2023-05-30 09:29:07 +1000

    Removed None GeoLocation static member

--- BackEnd/Domain/Models/GeoLocation.cs
+++ BackEnd/Domain/Models/GeoLocation.cs
@@ -3,19 +3,10 @@
 namespace OhMyWord.Domain.Models;
 
 public record GeoLocation
-{    
+{
     public required IPAddress IpAddress { get; init; }
     public required string CountryCode { get; init; }
     public required string CountryName { get; init; }
     public required string City { get; init; }
     public required DateTime LastUpdated { get; init; }
-
-    public static GeoLocation None => new()
-    {        
-        IpAddress = IPAddress.None,
-        CountryCode = string.Empty,
-        CountryName = string.Empty,
-        City = string.Empty,
-        LastUpdated = default
-    };
 }
