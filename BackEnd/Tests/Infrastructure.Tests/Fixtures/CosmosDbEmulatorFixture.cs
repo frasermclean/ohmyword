0e1ab06b7932041afa3e6e597ec154b8037b5889
Fraser McLean <dev@frasermclean.com>
2023-06-13 11:17:21 +1000

    Created CosmosDb emulator fixture and collection

---
+++ BackEnd/Tests/Infrastructure.Tests/Fixtures/CosmosDbEmulatorFixture.cs
@@ -0,0 +1,36 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+
+namespace Infrastructure.Tests.Fixtures;
+
+public sealed class CosmosDbEmulatorFixture : IAsyncLifetime
+{
+    private readonly IContainer container;
+
+    public CosmosDbEmulatorFixture()
+    {
+        container = new ContainerBuilder()
+            .WithImage("mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator")
+            .WithName("cosmosdb-emulator")
+            .WithExposedPort(8081)
+            .WithExposedPort(10251)
+            .WithExposedPort(10252)
+            .WithExposedPort(10253)
+            .WithExposedPort(10254)
+            .WithPortBinding(8081, true)
+            .WithPortBinding(10251, true)
+            .WithPortBinding(10252, true)
+            .WithPortBinding(10253, true)
+            .WithPortBinding(10254, true)
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_PARTITION_COUNT", "10")
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_IP_ADDRESS_OVERRIDE", "127.0.0.1")
+            .WithEnvironment("AZURE_COSMOS_EMULATOR_ENABLE_DATA_PERSISTENCE", "false")
+            .WithOutputConsumer(Consume.RedirectStdoutAndStderrToStream(new MemoryStream(), new MemoryStream()))
+            .WithWaitStrategy(Wait.ForUnixContainer()
+                .UntilMessageIsLogged("Started"))
+            .Build();
+    }
+
+    public Task InitializeAsync() => container.StartAsync();
+    public Task DisposeAsync() => container.DisposeAsync().AsTask();
+}
