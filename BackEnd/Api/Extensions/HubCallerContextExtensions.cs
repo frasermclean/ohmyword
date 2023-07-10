f00dfa37bfdfd7a9b64b77ef8431c6b1a0820451
Fraser McLean <dev@frasermclean.com>
2023-07-10 20:45:00 +1000

    Added Azure SignalR services

--- BackEnd/Api/Extensions/HubCallerContextExtensions.cs
+++ BackEnd/Api/Extensions/HubCallerContextExtensions.cs
@@ -7,6 +7,8 @@ namespace OhMyWord.Api.Extensions;
 
 public static class HubCallerContextExtensions
 {
+    private static readonly string[] ForwardHeaders = { "X-Forwarded-For", "X-Real-IP" };
+
     public static Guid? GetUserId(this HubCallerContext context)
         => Guid.TryParse(context.User?.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.NameIdentifier)?.Value,
             out var userId)
@@ -14,5 +16,21 @@ public static class HubCallerContextExtensions
             : null;
 
     public static IPAddress GetIpAddress(this HubCallerContext context)
-        => context.Features.GetRequiredFeature<IHttpConnectionFeature>().RemoteIpAddress ?? IPAddress.None;
+    {
+        var httpContext = context.GetHttpContext();
+
+        if (httpContext is null)
+            return IPAddress.None;
+
+        foreach (var header in ForwardHeaders)
+        {
+            if (httpContext.Request.Headers.TryGetValue(header, out var addresses) &&
+                IPAddress.TryParse(addresses.FirstOrDefault(), out var address))
+            {
+                return address;
+            }                
+        }
+        
+        return IPAddress.None;
+    }
 }
