baa76baad8b5d7afa34290749c8a3a4edcfdc4d0
Fraser McLean <dev@frasermclean.com>
2023-08-10 18:23:31 +1000

    Updated word queue service tests

--- BackEnd/Domain.Tests/Services/WordQueueServiceTests.cs
+++ BackEnd/Domain.Tests/Services/WordQueueServiceTests.cs
@@ -1,57 +1,60 @@
-﻿// using FluentResults;
-// using Microsoft.Extensions.Logging;
-// using OhMyWord.Core.Models;
-// using OhMyWord.Domain.Services;
-//
-// namespace OhMyWord.Domain.Tests.Services;
-//
-// public class WordQueueServiceTests
-// {
-//     private readonly IWordQueueService wordQueueService;
-//     private readonly Mock<IWordsService> wordsServiceMock = new();
-//
-//     public WordQueueServiceTests()
-//     {
-//         wordQueueService = new WordQueueService(Mock.Of<ILogger<WordQueueService>>(), wordsServiceMock.Object);
-//     }
-//
-//     [Theory, AutoData]
-//     public async Task GetNextWordAsync_Should_Return_ExpectedValues(string[] wordIds, Word expectedWord)
-//     {
-//         // arrange
-//         wordsServiceMock.Setup(service => service.GetAllWordIds(It.IsAny<CancellationToken>()))
-//             .Returns(wordIds.ToAsyncEnumerable());
-//         wordsServiceMock.Setup(service =>
-//                 service.GetWordAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()))
-//             .ReturnsAsync(Result.Ok(expectedWord));
-//
-//         // act
-//         var nextWord = await wordQueueService.GetNextWordAsync();
-//
-//         // assert
-//         nextWord.Should().Be(expectedWord);
-//         wordQueueService.TotalWordCount.Should().Be(wordIds.Length);
-//         wordQueueService.RemainingWordCount.Should().Be(wordIds.Length - 1);
-//         wordsServiceMock.Verify(service => service.GetAllWordIds(It.IsAny<CancellationToken>()), Times.Once);
-//         wordsServiceMock.Verify(
-//             service => service.GetWordAsync(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()),
-//             Times.Once);
-//     }
-//
-//     [Fact]
-//     public async Task GetNextWordAsync_WithNoWordsInDatabase_Should_Return_DefaultWord()
-//     {
-//         // arrange
-//         wordsServiceMock.Setup(service => service.GetAllWordIds(It.IsAny<CancellationToken>()))
-//             .Returns(AsyncEnumerable.Empty<string>());
-//
-//         // act
-//         var word = await wordQueueService.GetNextWordAsync();
-//
-//         // assert
-//         word.Should().Be(Word.Default);
-//         wordsServiceMock.Verify(service => service.GetAllWordIds(It.IsAny<CancellationToken>()), Times.Once);
-//     }
-// }
+﻿using Microsoft.Extensions.Logging;
+using OhMyWord.Core.Models;
+using OhMyWord.Domain.Services;
+
+namespace OhMyWord.Domain.Tests.Services;
+
+public class WordQueueServiceTests
+{
+    private readonly IWordQueueService wordQueueService;
+    private readonly IWordsService wordsService = Substitute.For<IWordsService>();
+
+    public WordQueueServiceTests()
+    {
+        wordQueueService = new WordQueueService(Substitute.For<ILogger<WordQueueService>>(), wordsService);
+    }
+
+    [Theory, AutoData]
+    public async Task GetNextWordAsync_Should_Return_ExpectedValues(string[] wordIds, Word expectedWord)
+    {
+        // arrange
+        SetupWordService(wordIds, expectedWord);
+
+        // act
+        var nextWord = await wordQueueService.GetNextWordAsync();
+
+        // assert
+        nextWord.Should().Be(expectedWord);
+        wordQueueService.TotalWordCount.Should().Be(wordIds.Length);
+        wordQueueService.RemainingWordCount.Should().Be(wordIds.Length - 1);
+        await wordsService.Received(1).GetAllWordIds(Arg.Any<CancellationToken>()).ToListAsync();
+        await wordsService.Received(1).GetWordAsync(Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<CancellationToken>());
+    }
 
 
+    [Fact]
+    public async Task GetNextWordAsync_WithNoWordsInDatabase_Should_Return_DefaultWord()
+    {
+        // arrange
+        SetupWordService();
+
+        // act
+        var word = await wordQueueService.GetNextWordAsync();
+
+        // assert
+        word.Should().Be(Word.Default);
+        await wordsService.Received(1).GetAllWordIds(Arg.Any<CancellationToken>()).ToListAsync();
+    }
+
+    private void SetupWordService(IEnumerable<string>? wordIds = default, Word? expectedWord = default)
+    {
+        wordIds ??= Enumerable.Empty<string>();
+        expectedWord ??= Word.Default;
+
+        wordsService.GetAllWordIds(Arg.Any<CancellationToken>())
+            .Returns(wordIds.ToAsyncEnumerable());
+
+        wordsService.GetWordAsync(Arg.Any<string>(), Arg.Any<bool>(), Arg.Any<CancellationToken>())
+            .Returns(expectedWord);
+    }
+}
