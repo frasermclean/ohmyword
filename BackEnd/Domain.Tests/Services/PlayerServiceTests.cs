dcce0d9a404b13d70ea3ccfafb30e4d35040fb90
Fraser McLean <dev@frasermclean.com>
2023-08-10 15:48:53 +1000

    Updated player service tests

--- BackEnd/Domain.Tests/Services/PlayerServiceTests.cs
+++ BackEnd/Domain.Tests/Services/PlayerServiceTests.cs
@@ -24,7 +24,7 @@ public class PlayerServiceTests
 
     [Theory, AutoData]
     public async Task GetPlayerAsync_With_NewPlayerId_Should_ReturnExpected(Guid playerId, string visitorId,
-        string connectionId, IPAddress ipAddress, Guid? userId, string name, GeoLocation geoLocation)
+        string connectionId, IPAddress ipAddress, Guid userId, string name, GeoLocation geoLocation)
     {
         // arrange
         playerRepository.GetPlayerByIdAsync(playerId, Arg.Any<CancellationToken>())
@@ -33,11 +33,8 @@ public class PlayerServiceTests
         playerRepository.CreatePlayerAsync(Arg.Any<PlayerEntity>(), Arg.Any<CancellationToken>())
             .Returns(info => info.Arg<PlayerEntity>());
 
-        graphApiClient.GetUserByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
-            .Returns(info => new User { Id = info.Arg<Guid>().ToString(), GivenName = name });
-
-        geoLocationService.GetGeoLocationAsync(Arg.Any<IPAddress>(), Arg.Any<CancellationToken>())
-            .Returns(geoLocation);
+        SetupGraphApiClient(userId, name);
+        SetupGeoLocationService(ipAddress, geoLocation);
 
         // act
         var player = await playerService.GetPlayerAsync(playerId, visitorId, connectionId, ipAddress, userId);
@@ -53,51 +50,59 @@ public class PlayerServiceTests
         player.RegistrationCount.Should().Be(1);
         player.GeoLocation.Should().Be(geoLocation);
     }
-}
 
-//     [Theory, AutoData]
-//     public async Task GetPlayerAsync_With_ExistingPlayerId_Should_ReturnExpected(Guid playerId, string visitorId,
-//         string connectionId, IPAddress ipAddress, Guid? userId, string name, GeoLocation geoLocation)
-//     {
-//         // arrange
-//         playerRepositoryMock.Setup(repository =>
-//                 repository.GetPlayerByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-//             .ReturnsAsync((Guid id, CancellationToken _) => new PlayerEntity
-//             {
-//                 Id = id.ToString(),
-//                 UserId = userId,
-//                 RegistrationCount = 3,
-//                 Score = 123,
-//                 VisitorIds = new[] { visitorId },
-//                 IpAddresses = new[] { ipAddress.ToString() },
-//             });
-//
-//         playerRepositoryMock.Setup(repository =>
-//                 repository.UpdatePlayerAsync(It.IsAny<PlayerEntity>(), It.IsAny<string>(), It.IsAny<string>()))
-//             .ReturnsAsync((PlayerEntity entity, string _, string _) => entity with
-//             {
-//                 RegistrationCount = entity.RegistrationCount + 1
-//             });
-//
-//         graphApiClientMock.Setup(client => client.GetUserByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-//             .ReturnsAsync((Guid id, CancellationToken _) => new User { Id = id.ToString(), GivenName = name });
-//
-//         geoLocationServiceMock.Setup(service =>
-//                 service.GetGeoLocationAsync(It.IsAny<IPAddress>(), It.IsAny<CancellationToken>()))
-//             .ReturnsAsync(geoLocation);
-//
-//         // act
-//         var player = await playerService.GetPlayerAsync(playerId, visitorId, connectionId, ipAddress, userId);
-//
-//         // assert
-//         player.Should().NotBeNull();
-//         player.Id.Should().Be(playerId);
-//         player.Name.Should().Be(name);
-//         player.ConnectionId.Should().Be(connectionId);
-//         player.UserId.Should().Be(userId);
-//         player.Score.Should().Be(123);
-//         player.RegistrationCount.Should().Be(4);
-//         player.VisitorId.Should().Be(visitorId);
-//         player.GeoLocation.Should().Be(geoLocation);
-//     }
-// }
+
+    [Theory, AutoData]
+    public async Task GetPlayerAsync_With_ExistingPlayerId_Should_ReturnExpected(Guid playerId, string visitorId,
+        string connectionId, IPAddress ipAddress, Guid userId, string name, GeoLocation geoLocation)
+    {
+        // arrange
+        var playerEntity = new PlayerEntity()
+        {
+            Id = playerId.ToString(),
+            UserId = userId,
+            RegistrationCount = 3,
+            Score = 123,
+            VisitorIds = new[] { visitorId },
+            IpAddresses = new[] { ipAddress.ToString() },
+        };
+        SetupPlayerRepository(playerId, visitorId, ipAddress, playerEntity);
+        SetupGraphApiClient(userId, name);
+        SetupGeoLocationService(ipAddress, geoLocation);
+
+        // act
+        var player = await playerService.GetPlayerAsync(playerId, visitorId, connectionId, ipAddress, userId);
+
+        // assert
+        player.Should().NotBeNull();
+        player.Id.Should().Be(playerId);
+        player.Name.Should().Be(name);
+        player.ConnectionId.Should().Be(connectionId);
+        player.UserId.Should().Be(userId);
+        player.Score.Should().Be(123);
+        player.RegistrationCount.Should().Be(4);
+        player.VisitorId.Should().Be(visitorId);
+        player.GeoLocation.Should().Be(geoLocation);
+    }
+
+    private void SetupPlayerRepository(Guid playerId, string visitorId, IPAddress ipAddress, PlayerEntity playerEntity)
+    {
+        playerRepository.GetPlayerByIdAsync(playerId, Arg.Any<CancellationToken>())
+            .Returns(playerEntity);
+
+        playerRepository.UpdatePlayerAsync(playerEntity, visitorId, ipAddress.ToString())
+            .Returns(playerEntity with { RegistrationCount = playerEntity.RegistrationCount + 1 });
+    }
+
+    private void SetupGraphApiClient(Guid userId, string name)
+    {
+        graphApiClient.GetUserByIdAsync(userId, Arg.Any<CancellationToken>())
+            .Returns(info => new User { Id = userId.ToString(), GivenName = name });
+    }
+
+    private void SetupGeoLocationService(IPAddress ipAddress, GeoLocation geoLocation)
+    {
+        geoLocationService.GetGeoLocationAsync(ipAddress, Arg.Any<CancellationToken>())
+            .Returns(geoLocation);
+    }
+}
